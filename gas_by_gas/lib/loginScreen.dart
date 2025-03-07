2081fe840f833b013aa54aa06d3eaad9e6ed9f55
Pasindu Ekanayake <pasinduekanayaka99@gmail.com>
2025-03-07 20:37:11 +0530

    Update loginScreen.dart

--- gas_by_gas/lib/loginScreen.dart
+++ gas_by_gas/lib/loginScreen.dart
@@ -12,8 +12,8 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
-  String _email = '';
-  String _password = '';
+  String _mobile = '';
+  // String _password = '';
   bool _obscureText = true;
 
   void _togglePasswordVisibility() {
@@ -79,37 +79,37 @@ class _LoginScreenState extends State<LoginScreen> {
                             borderRadius: BorderRadius.circular(10),
                           ),
                         ),
-                        onChanged: (value) => _email = value,
+                        onChanged: (value) => _mobile = value,
                         validator:
                             (value) =>
                                 value!.isEmpty
-                                    ? "Please enter your user name"
+                                    ? "Please enter your mobile number"
                                     : null,
                       ),
-                      const SizedBox(height: 16),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: "Password",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                              _obscureText
-                                  ? Icons.visibility_off
-                                  : Icons.visibility,
-                            ),
-                            onPressed: _togglePasswordVisibility,
-                          ),
-                        ),
-                        onChanged: (value) => _password = value,
-                        validator:
-                            (value) =>
-                                value!.isEmpty
-                                    ? "Please enter your password"
-                                    : null,
-                        obscureText: _obscureText,
-                      ),
+                      // const SizedBox(height: 16),
+                      // TextFormField(
+                      //   decoration: InputDecoration(
+                      //     labelText: "Password",
+                      //     border: OutlineInputBorder(
+                      //       borderRadius: BorderRadius.circular(10),
+                      //     ),
+                      //     suffixIcon: IconButton(
+                      //       icon: Icon(
+                      //         _obscureText
+                      //             ? Icons.visibility_off
+                      //             : Icons.visibility,
+                      //       ),
+                      //       onPressed: _togglePasswordVisibility,
+                      //     ),
+                      //   ),
+                      //   onChanged: (value) => _password = value,
+                      //   validator:
+                      //       (value) =>
+                      //           value!.isEmpty
+                      //               ? "Please enter your password"
+                      //               : null,
+                      //   obscureText: _obscureText,
+                      // ),
                       const SizedBox(height: 62),
                       ElevatedButton(
                         style: ElevatedButton.styleFrom(
